63965f0f934aad91f9eecfa4acc277391ea039f1
Harsh Kumar <113852094+h4rshhh@users.noreply.github.com>
2023-05-14 14:14:44 +0530

    Create reverseofnum.cpp
    [ASSIGNMENT3]
    Write a program to generate the reverse of a given number N. Print the corresponding reverse number.
    Note : If a number has trailing zeros, then its reverse will not include them. For e.g., reverse of 10400 will be 401 instead of 00401.
    
    Input format :
    Integer N
    Output format :
    Corresponding reverse number

---
+++ reverseofnum.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){ 
+    int n;
+    cin>>n;
+
+    int a = 0;
+    while(n>0){
+        a = a*10 + n%10;
+        n = n/10;
+    }
+    cout<<a;
+    
+    return 0;
+}
